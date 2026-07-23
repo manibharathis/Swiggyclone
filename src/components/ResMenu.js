31700ac917247735e544530e12a722a46ed86fa8
manibharathis <remasekar.96@gmail.com>
2026-07-23 11:10:08 +0530

    lifting state up

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -4,20 +4,23 @@ import useFetchMenu from "../utils/useFetchMenu";
 import MenuItem from "./MenuItems";
 import MenuCategory from "./MenuCategory";
 const ResMenu = () => {
-  
+  const [showItem,setShowItem] = useState(false)
+  const [showIndex,setShowIndex] = useState(0)
   let params = useParams();
   let id = params.id;
-  const resMenu = useFetchMenu({ id });
+  const resMenu = useFetchMenu({
+     id });
+     const setIndex = (index)=>{setShowIndex(index)}
   return (
     
     <div className="ml-96 w-2xl">
       <h2 className="font-bold text-2xl">Menu</h2>
-      {resMenu.map((card) => {
+      {resMenu.map((card,index) => {
         let items = card?.card?.card?.itemCards;
         let title = card?.card?.card?.title;
         if (!items) return null;
         return (
-          <MenuCategory key={title} title={title} items={items}/>
+          <MenuCategory key={title} index={index} setIndex={setIndex} showIndex={showIndex}  title={title} items={items}/>
          
         );
       })}
